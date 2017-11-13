aab1dda25671ed20490815c5f1132de384d304b0
SamualAnnis-Brown <samual@annisbrown.com>
2017-11-13 22:58:21 +1100

    Adding Polly

--- core/polly.py
+++ core/polly.py
@@ -4,24 +4,29 @@ from core.connection import *
 from core.s3 import *
 
 
-def polly_talk(textin):
+def polly_talk(testtospeech, filename='labaucielPollyOutput.mp3', bucket=get_config_item('s3_bucket_name')):
     client = connect_boto_client('polly', 'us-west-2')
-    print(textin)
 
+    # Get response from Polly
     response = client.synthesize_speech(
         OutputFormat='mp3',
-        Text=textin,
+        Text=testtospeech,
         VoiceId='Geraint'
     )
 
+    # Stream response to variable
     if 'AudioStream' in response:
         with closing(response['AudioStream']) as stream:
-            output = "polly-boto.mp3"
-    print(response['AudioStream'])
-    s3.put_object('labauciel-pollyout', 'doesitwork.mp3', response['AudioStream'])
+            output = stream.read()
 
-    audiofile = 'placeholder'
-    return audiofile
+    # Write object to S3
+    s3.put_object(bucket, filename, output)
+
+    s3link = 'https://s3-' + get_config_item('default_region') + '.amazonaws.com/' + bucket + "/" + filename
+
+    print(s3link)
+    # Returns link to S3 bucket
+    return s3link
 
 
 def polly_voices():
--- core/s3.py
+++ core/s3.py
@@ -2,18 +2,36 @@ from .connection import *
 
 
 class s3():
-    def put_object(bucket, key, data):
-        client = connect_boto_client('s3', get_config_item('default_region'))
+    def put_object(bucket, key, data, region=get_config_item('default_region')):
+        client = connect_boto_client('s3', region)
 
-        client.put_object(
+        object = client.put_object(
             Bucket=bucket,
             Key=key,
-            Body=data
+            Body=data,
         )
 
+        object.set_acl('public-read')
 
-def create_default_config():
-    print()
+    def list_objects(bucket, region=get_config_item('default_region')):
+        client = connect_boto_client('s3', region)
+
+        output = client.list_objects_v2(
+            Bucket=bucket
+        )
+
+        return output
+
+    def get_object(bucket, key, region=get_config_item('default_region')):
+        client = connect_boto_client('s3', region)
+
+        output = client.get_object(
+            Bucket=bucket,
+            Key=key
+        )
+
+        return output
+
 
 
 
