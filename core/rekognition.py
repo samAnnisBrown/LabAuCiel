b8da7926a675d45e6ccdffa55b5a705d762f8685
SamualAnnis-Brown <samual@annisbrown.com>
2017-11-15 19:48:36 +1100

    Rekog - upload to s3 working

--- core/rekognition.py
+++ core/rekognition.py
@@ -6,8 +6,17 @@ class rekog():
     @staticmethod
     def detectObject(image):
 
-        #print(image)
+        client = connect_boto_client('rekognition', 'us-west-2')
 
-        s3.putObject(get_config_item('s3_bucket_name'), 'rekognition/latest.jpeg', image)
+        response = client.detect_labels(
+            Image={
+                'Bytes': image,
+            },
+            MaxLabels=1,
+        )
+
+        print("I\'m " + str(response['Labels'][0]['Confidence']) + " confident that I\'m looking at. " + str(response['Labels'][0]['Name']))
+
+        #s3.putObject(get_config_item('s3_bucket_name'), 'rekognition/latest.jpeg', image)
 
 
