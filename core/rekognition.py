f3384f460b3f1292747d0331edea0a4745ee4b71
SamualAnnis-Brown <samual@annisbrown.com>
2017-11-16 09:52:28 +1100

    Rekog - upload to s3 working

--- core/rekognition.py
+++ core/rekognition.py
@@ -16,31 +16,61 @@ class rekog():
             MaxLabels=1,
         )
 
+
         #s3.putObject(get_config_item('s3_bucket_name'), 'rekognition/latest.jpeg', image)
 
-        if response['Labels'][0]['Name'] == 'People':
+        # If the response is a person
+        if response['Labels'][0]['Name'] == 'People' or response['Labels'][0]['Name'] == 'Human':
 
-            response = client.detect_faces(
+            # Check to see if it's a celebrity
+            celeb = client.recognize_celebrities(
                 Image={
                     'Bytes': image,
-                },
-                Attributes=[
-                    'ALL',
-                ]
+                }
             )
 
+            print(celeb)
+            celebexist = True
 
-            gender = response['FaceDetails'][0]['Gender']['Value']
-            smiling = response['FaceDetails'][0]['Smile']['Value']
-            emotion = response['FaceDetails'][0]['Emotions'][0]['Type']
+            try:
+                celeb['CelebrityFaces'][0]['Name']
+            except:
+                celebexist = False
 
-            if smiling:
-                text = 'This person is ' + gender + ', and it looks like they\'re ' + emotion + " and smiling"
+            # If a celebrity is found
+            if celebexist:
+
+                celebname = celeb['CelebrityFaces'][0]['Name']
+                celebconfidence = str(round(celeb['CelebrityFaces'][0]['Face']['Confidence'], 1))
+
+                text = "I\'m " + celebconfidence + " percent confident that I\'m looking at " + celebname
+                print(text)
+
+            # If not a celebrity, get details on the face
             else:
-                text = 'This person is ' + gender + ', and it looks like they\'re ' + emotion + ".  They're not smiling"
 
-            print(text)
-            print(response)
+                person = client.detect_faces(
+                    Image={
+                        'Bytes': image,
+                    },
+                    Attributes=[
+                        'ALL',
+                    ]
+                )
+
+
+                gender = person['FaceDetails'][0]['Gender']['Value']
+                smiling = person['FaceDetails'][0]['Smile']['Value']
+                emotion = person['FaceDetails'][0]['Emotions'][0]['Type']
+
+                if smiling:
+                    text = 'This person is ' + gender + ', and it looks like they\'re ' + emotion + " and smiling"
+                else:
+                    text = 'This person is ' + gender + ', and it looks like they\'re ' + emotion + ".  They're not smiling"
+
+                print(text)
+                print(person)
+        # If it's not a person, return object details
         else:
             text = "I\'m " + str(round(response['Labels'][0]['Confidence'], 1)) + " percent confident that I\'m looking at a " + str(response['Labels'][0]['Name'])
             print(text)
