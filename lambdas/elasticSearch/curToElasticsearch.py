5b6449b676d3f4f3774745dee27e140e3d0a2524
ansamual <ansamual@amazon.com>
2018-02-10 18:04:19 +1100

    Working on arguments and uploads

--- lambdas/elasticSearch/curToElasticsearch.py
+++ lambdas/elasticSearch/curToElasticsearch.py
@@ -247,7 +247,7 @@ if args.latest:
     get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
     objs = s3.list_objects_v2(Bucket=args.bucket)['Contents']
     [obj['Key'] for obj in sorted(objs, key=get_last_modified)]
-    print(objs[0])
+    print(objs[0-10])
 
     sys.exit()
 
