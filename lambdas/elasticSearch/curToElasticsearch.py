87edf5e5af4e1f96f5580787796403a29227ad35
ansamual <ansamual@amazon.com>
2018-02-21 19:25:43 +1100

    Working on arguments and uploads

--- lambdas/elasticSearch/curToElasticsearch.py
+++ lambdas/elasticSearch/curToElasticsearch.py
@@ -54,6 +54,8 @@ parser.add_argument('--dryrun',
 
 args = parser.parse_args()
 
+gzipFiles = []
+
 try:
     if args.customer.lower() == 'rmit':
         args.role_arn = 'arn:aws:iam::182132151869:role/AWSEnterpriseSupportCURAccess'
@@ -316,31 +318,32 @@ if customerImport:
     sortedCur = sorted(curFiles.items(), key=operator.itemgetter(1), reverse=True)
     print('File ' + sortedCur[0][0] + ' chosen with date ' + sortedCur[0][1])
     folderHash = re.search(".*/(.+-.+-.+-.+)/.+", sortedCur[0][0]).group(1)
-    newlist = []
+
 
     for listObjectsOutput in outputList:
         for s3Object in listObjectsOutput['Contents']:
             if 'csv.gz' in s3Object['Key'] and folderHash in s3Object['Key']:
-                newlist.append(s3Object['Key'])
-    print(newlist)
+                gzipFiles.append(s3Object['Key'])
+    print(gzipFiles)
     args.key = sortedCur[0][0]
-    sys.exit()
+
 
 if args.cur_load:  # If not in a Lambda, launch main function and pass S3 event JSON
     if args.bucket is None or args.key is None:
         print('Set both --bucket and --key need to location of the CUR file in S3 you want to import.')
     else:
-        lambda_handler({
-            "Records": [
-                {
-                    "s3": {
-                        "bucket": {
-                            "name": args.bucket,
-                        },
-                        "object": {
-                            "key": args.key,
+        for key in gzipFiles:
+            lambda_handler({
+                "Records": [
+                    {
+                        "s3": {
+                            "bucket": {
+                                "name": args.bucket,
+                            },
+                            "object": {
+                                "key": key,
+                            }
                         }
                     }
-                }
-            ]
-        }, "")
+                ]
+            }, "")
