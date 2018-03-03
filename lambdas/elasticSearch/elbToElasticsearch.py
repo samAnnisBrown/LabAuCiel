52767fa51fb357a7fba8bd82001b1037eee46052
ansamual <ansamual@amazon.com>
2018-03-03 19:08:45 +1100

    Create Athena Transform

--- lambdas/elasticSearch/elbToElasticsearch.py
+++ lambdas/elasticSearch/elbToElasticsearch.py
@@ -11,11 +11,10 @@ from time import sleep
 # Elasticsearch host name
 esHost = "vpc-aws-cost-analysis-hmr7dskev6kmznsmqzhmv7r3te.ap-southeast-2.es.amazonaws.com"
 indexName = 'elb-accesslogs'
-esBulkUrl = "https://" + esHost + "/_bulk"
 
 
 def lambda_handler(event, context):
-    sleep(1)
+    sleep(1)    # Attach ENI
     # Retrieve S3 Bucket/Key from event
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = event["Records"][0]["s3"]["object"]["key"]
@@ -27,7 +26,8 @@ def lambda_handler(event, context):
         Key=key
     )
 
-    if '.log.gz' in key:    # ALB or NLB
+    # Determine if ALB or ELB - set appropriate variabls
+    if '.log.gz' in key:    # ALB
         print('ALB Log File')
         bytestream = BytesIO(s3file['Body'].read())
         body = GzipFile(None, 'rb', fileobj=bytestream).read().decode('utf-8')
@@ -53,11 +53,11 @@ def lambda_handler(event, context):
                    "ssl_version",
                    "additional_info"
                    ]
-        albRegex = '^(.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+):(\d+) (.[^ ]+):(\d+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (\d+) (\d+) "(\w+) (.[^ ]+) (.[^ ]+)" "(.+?)"(.[^ ]+) (.[^ ]+) (.+)\n'
+        albRegex = '^(.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+):(\\d+) (.[^ ]+):(\\d+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (\\d+) (\\d+) \"(\\w+) (.[^ ]+) (.[^ ]+)\" \"(.+?)\"(.[^ ]+) (.[^ ]+) (.+)'
         regex = re.compile(albRegex)
     else:
         print('CLB Log File')
-        body = s3file['Body'].read()
+        body = s3file["Body"].read().decode("utf-8")
         columns = ["timestamp",
                    "elb",
                    "client_ip",
@@ -78,10 +78,10 @@ def lambda_handler(event, context):
                    "ssl_cipher",
                    "ssl_version"
                    ]
-        clbRegex = '^(.[^ ]+) (.[^ ]+) (.[^ ]+):(\d+) (.[^ ]+):(\d+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (\d+) (\d+) "(\w+) (.[^ ]+) (.[^ ]+)" "(.+?)"(.[^ ]+) (.[^ ]+)\n'
+        clbRegex = '^(.[^ ]+) (.[^ ]+) (.[^ ]+):(\\d+) (.[^ ]+):(\\d+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (\\d+) (\\d+) \"(\\w+) (.[^ ]+) (.[^ ]+)\" \"(.+?)\"(.[^ ]+) (.+)'
         regex = re.compile(clbRegex)
 
-    # Retrieve Access details (Lambda IAM role must have access to ES domain to work)
+    # Auth (Lambda IAM role must have access to ES domain)
     awsauth = AWSRequestsAuth(aws_access_key=os.environ['AWS_ACCESS_KEY_ID'],
                               aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
                               aws_token=os.environ['AWS_SESSION_TOKEN'],
@@ -94,30 +94,50 @@ def lambda_handler(event, context):
                        connection_class=RequestsHttpConnection,
                        http_auth=awsauth)
 
-    actions = []
+    linesToUpload = []
 
     # For each line in the loaded S3 file
     for line in body.strip().split("\n"):
-        # Match the Regex to the line, outputting an array
-        regexMatch = regex.match(line)
-        if not regexMatch:
-            continue
+        regexMatch = regex.match(line)      # Match the Regex to the line, outputting an array
+        valuesList = regexMatch.groups(0)   # Get the Regex Groups
 
-        valuesList = regexMatch.groups(0)
+        # Force these columns to be numbers (so we can do math on them in ES)
+        forceFloat = ["request_processing_time",
+                      "backend_processing_time",
+                      "response_processing_time",
+                      "received_bytes",
+                      "sent_bytes"
+                      ]
+        floatIndexNumbers = []
+        for i, k in enumerate(columns):
+            if k in forceFloat:
+                floatIndexNumbers.append(i)
 
-        # Merge column title list 'albkeys' and 'valuesList' list into dictionary
-        payload = dict(zip(columns, valuesList))
-        actions.append({"_index": indexName, "_type": 'doc', "_source": payload})
+        # Build the ES payload, setting columns above to numbers, and anything else to strings
+        payloadOut = []
+        for index, value in enumerate(valuesList):
+            if index in floatIndexNumbers:
+                try:
+                    payloadOut.append(float(value))
+                except:
+                    payloadOut.append(0.0)
+            else:
+                # Anything that's not integer or float is created as a String
+                payloadOut.append(value)
 
-        # If greater than 1000 actions, commit bulk upload
-        if len(actions) > 250:
-            helpers.bulk(es, actions)
-            actions = []
+        # Merge column list payload list into dictionary
+        payload = dict(zip(columns, payloadOut))
+        linesToUpload.append({"_index": indexName, "_type": 'doc', "_source": payload})
+
+        # If greater than 250 linesToUpload, commit bulk upload
+        if len(linesToUpload) > 250:
+            helpers.bulk(es, linesToUpload)
+            linesToUpload = []
             print('Uploading 250 lines to ' + indexName)
 
-    # When loop ends, upload whatever actions remain in final bulk upload
-    if len(actions) > 0:
-        helpers.bulk(es, actions)
+    # When loop ends, upload whatever linesToUpload remain in final bulk upload
+    if len(linesToUpload) > 0:
+        helpers.bulk(es, linesToUpload)
         print('Finished uploading.')
 
 
