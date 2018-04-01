85c7fb6491b59b7e2b9fb89ee11d1b6e5a7ed197
ansamual <ansamual@amazon.com>
2018-04-01 16:47:58 +1000

    Updated Endpoint

--- lambdas/other/cfnCustomResource.py
+++ lambdas/other/cfnCustomResource.py
@@ -1,6 +1,27 @@
 import boto3
+import cfnresponse
 
-ec2 = boto3.resource('ec2')
-vpc = ec2.Vpc('vpc-445fb623')
-response = vpc.cidr_block
-print(response)
+def lambda_handler(event, context):
+    try:
+        instance = event['ResourceProperties']['InstanceId']
+        region = event['ResourceProperties']['Region']
+
+        client = boto3.client('ec2', region_name=region)
+
+        getvolume = client.describe_instances(
+            InstanceIds=[instance],
+        )
+
+        createami = client.create_image(
+            InstanceId=instance,
+            Name=instance,
+            NoReboot=True
+        )
+        responseData = {'AmiId': createami['ImageId'], 'VolumeId': getvolume['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['VolumeId']}
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+    except:
+        cfnresponse.send(event, context, cfnresponse.FAILED)
+
+
+lambda_handler('', '')
+
