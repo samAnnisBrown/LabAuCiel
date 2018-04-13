7eb7c6202815ba4aa466e2ef09db3cef262af960
EC2 Default User <ec2-user@ip-172-31-18-181.us-west-2.compute.internal>
2018-04-13 05:38:36 +0000

    copyCur

--- projects/cur_transform/copyCur.py
+++ projects/cur_transform/copyCur.py
@@ -1,29 +1,88 @@
 import boto3
 import re
+import json
 
 
 def lambda_handler(event, context):
 
     bucketSrc = 'ansamual-costreports'
-    bucketDst = 'ansamual-cur-clean'
+    bucketDst = 'ansamual-cur-sorted'
     prefix = 'QuickSight_RedShift'
     report = 'QuickSight_RedShift_CostReports'
     region = 'ap-southeast-2'
-    # Retrieve S3 object from event
-    #bucket = event["Records"][0]["s3"]["bucket"]["name"]
-    #key = event["Records"][0]["s3"]["object"]["key"]
+    roleArn = ''
 
-    s3 = boto3.client('s3', region_name=region)
-    objects = s3.list_objects_v2(Bucket=bucketSrc,
+    s3c = getS3Auth(region, 'client')
+    s3r = getS3Auth(region, 'resource')
+    
+    objects = s3c.list_objects_v2(Bucket=bucketSrc,
                                  Delimiter='/',
                                  Prefix=prefix + '/' + report + '/')
 
     for prefix in objects['CommonPrefixes']:
         try:
-            searchKey = re.search(".+/(\d+)-", prefix['Prefix']).group(1)
-            print(searchKey)
+            reportMonth = re.search(".+/(\d+)-", prefix['Prefix']).group(1)
+            manifestLocation = prefix['Prefix'] + report + '-Manifest.json'
+            manifestFile = s3c.get_object(Bucket=bucketSrc, Key=manifestLocation)
+            manifestFileContents = manifestFile['Body'].read().decode('utf-8')
+            manifestJsonContents = json.loads(manifestFileContents)
+
+            for keySrc in manifestJsonContents['reportKeys']:
+                objectName = re.search(".+/(.*)", keySrc).group(1)
+                objectSrc = { 'Bucket': bucketSrc, 'Key': keySrc }
+                objectDst = report + '/' + reportMonth + '/' + objectName
+                bucket = s3r.Bucket(bucketDst)
+                print('[COPYING]\nFROM: s3://' + bucketSrc + '/' + manifestLocation + '\nTO: s3://' + bucketDst + '/' + objectDst)
+                bucket.copy(objectSrc, objectDst)
+            
         except Exception as e:
             pass
 
 
-lambda_handler(None, None)
+def getS3Auth(region, accessType, roleArn):
+    if roleArn is not None:
+        client = boto3.client('sts')
+        assumed_role = client.assume_role(
+            RoleArn=roleArn,
+            RoleSessionName='cur_temp_sts_session'
+        )
+
+        creds = assumed_role['Credentials']
+        print(creds)
+        
+        if accessType == 'client':
+            s3 = boto3.client('s3',
+                              region_name=region,
+                              aws_access_key_id=creds['AccessKeyId'],
+                              aws_secret_access_key=creds['SecretAccessKey'],
+                              aws_session_token=creds['SessionToken'], )
+        
+        elif accessType == 'resource':
+            s3 = boto3.resource('s3',
+                              region_name=region,
+                              aws_access_key_id=creds['AccessKeyId'],
+                              aws_secret_access_key=creds['SecretAccessKey'],
+                              aws_session_token=creds['SessionToken'], )
+    else:
+        if accessType == 'client':
+            s3 = boto3.client('s3', region_name=region)
+        
+        elif accessType == 'resource':
+            s3 = boto3.resource('s3', region_name=region)
+
+    return s3
+    
+
+def testing():
+    bucketSrc = 'rmit-billing-report'
+    region = 'ap-southeast-2'
+    roleArn = 'arn:aws:iam::182132151869:role/AWSEnterpriseSupportCURAccess'
+    
+    s3c = getS3Auth(region, 'client', roleArn)
+
+    
+    objects = s3c.list_objects_v2(Bucket=bucketSrc)
+    
+testing()
+
+#lambda_handler(None, None)
